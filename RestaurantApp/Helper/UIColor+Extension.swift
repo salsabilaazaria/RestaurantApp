3f259563dab4ad183d3d810235bb4d9d5ae27a00
Salsabila Azaria <salsabila.azaria@dana.id>
2022-12-20 14:10:29 +0700

    update ui color

--- RestaurantApp/Helper/UIColor+Extension.swift
+++ RestaurantApp/Helper/UIColor+Extension.swift
@@ -10,11 +10,11 @@ import UIKit
 extension UIColor {
 	
 	public class var bukaRestoDarkGreen: UIColor {
-		return UIColor(red: 72, green: 130, blue: 126, alpha: 1)
+		return UIColor(red: 72/255.0, green: 130/255.0, blue: 126/255.0, alpha: 1)
 	}
 	
 	public class var bukaRestoLightGray: UIColor {
-		return UIColor(red: 135, green: 76, blue: 98, alpha: 1)
+		return UIColor(red: 135/255.0, green: 76/255.0, blue: 98/255.0, alpha: 1)
 	}
 
 }
@@ -22,6 +22,6 @@ extension UIColor {
 extension UIColor {
 	//FONT COLOR
 	public class var bukaRestoSecondaryFontColor: UIColor {
-		return UIColor(red: 79, green: 79, blue: 79, alpha: 1)
+		return UIColor(red: 79/255.0, green: 79/255.0, blue: 79/255.0, alpha: 1)
 	}
 }
