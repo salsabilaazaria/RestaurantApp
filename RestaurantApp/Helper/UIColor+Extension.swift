27a82f436364b3e5b7cf52c001446985e567ef2a
Maulana Dimas <maulana.iffandi@dana.id>
2022-12-23 17:40:01 +0700

    add new helper

--- RestaurantApp/Helper/UIColor+Extension.swift
+++ RestaurantApp/Helper/UIColor+Extension.swift
@@ -17,11 +17,20 @@ extension UIColor {
 		return UIColor(red: 242/255.0, green: 240/255.0, blue: 240/255.0, alpha: 1)
 	}
 
+	//Disabled button
+	public class var bukaRestoDisabledButton: UIColor {
+		return UIColor(red: 125/255.0, green: 125/255.0, blue: 125/255.0, alpha: 1)
+	}
+
 }
 
 extension UIColor {
 	//FONT COLOR
 	public class var bukaRestoSecondaryFontColor: UIColor {
 		return UIColor(red: 79/255.0, green: 79/255.0, blue: 79/255.0, alpha: 1)
+	}
+	
+	public class var bukaRestoErrorFontColor: UIColor {
+		return UIColor(red: 255/255.0, green: 137/255.0, blue: 137/255.0, alpha: 1)
 	}
 }
