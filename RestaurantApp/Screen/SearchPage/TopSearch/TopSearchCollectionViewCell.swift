28b7e149a483290ec3f23b1ba599d8b1530e1571
Salsabila Azaria <salsabila.azaria@dana.id>
2022-12-29 18:39:01 +0700

    create top search collection view cell

---
+++ RestaurantApp/Screen/SearchPage/TopSearch/TopSearchCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TopSearchCollectionViewCell.swift
+//  RestaurantApp
+//
+//  Created by Salsabila Azaria on 28/12/22.
+//
+
+import UIKit
+
+class TopSearchCollectionViewCell: UICollectionViewCell {
+
+	@IBOutlet weak var topSearchLabel: UILabel!
+	
+	override func awakeFromNib() {
+        super.awakeFromNib()
+		backgroundColor = .white
+        // Initialization code
+    }
+	
+	func setTopSearchLabel(text: String){
+		topSearchLabel.attributedText = NSAttributedString.body(text: text)
+	}
+
+}
