a26dccae129062e1703807e1c850f0d5482935fb
Salsabila Azaria <salsabila.azaria@dana.id>
2023-01-20 23:17:27 +0700

    fix scroll behaviour

--- RestaurantApp/Screen/DetailPage/Menu/MenuSectionTableView.swift
+++ RestaurantApp/Screen/DetailPage/Menu/MenuSectionTableView.swift
@@ -8,17 +8,26 @@
 import UIKit
 
 class MenuSectionTableView: UITableViewCell {
+	var onReachTop: (() -> Void)?
+	var onScrollToIndex: (() -> Void)?
 	
 	@IBOutlet weak var menuTableView: UITableView!
 	let identifier = "MenuSectionTableView"
 	
 	private let menuCellIdentifier = MenuTableViewCell().identifier
+	var mainScrollView: UIScrollView?
 	
 	var restoMenu: [RestoMenu]? = nil {
 		didSet {
 			DispatchQueue.main.async {
 				self.menuTableView.reloadData()
 			}
+		}
+	}
+	
+	var isScrollEnabled: Bool = false {
+		didSet {
+			self.menuTableView.isScrollEnabled = isScrollEnabled
 		}
 	}
 	
@@ -32,7 +41,17 @@ class MenuSectionTableView: UITableViewCell {
 		menuTableView.dataSource = self
 		menuTableView.sectionHeaderTopPadding = 0
 		menuTableView.register(UINib(nibName: menuCellIdentifier, bundle: nil), forCellReuseIdentifier: menuCellIdentifier)
-		menuTableView.isUserInteractionEnabled = false
+		menuTableView.isScrollEnabled = false
+		//buat ilangin putih2 diatas setelah di scroll ke bawah
+		menuTableView.insetsLayoutMarginsFromSafeArea = false
+	}
+	
+	func scrollToIndexPath(indexPath: IndexPath) {
+		DispatchQueue.main.async {
+			self.menuTableView.reloadSections(IndexSet(integer: indexPath.section), with: .top)
+			self.menuTableView.scrollToRow(at: indexPath, at: .top, animated: true)
+
+		}
 	}
 }
 
@@ -69,5 +88,40 @@ extension MenuSectionTableView: UITableViewDataSource {
 	}
 	
 	
+}
+
+extension MenuSectionTableView: UIScrollViewDelegate {
+	func scrollViewDidScroll(_ scrollView: UIScrollView) {
+		let yContentOffSet = scrollView.contentOffset.y
+		
+		if yContentOffSet <= 0 {
+			let delta = abs(yContentOffSet )
+			var newCellOffSet = (mainScrollView?.contentOffset.y ?? 0) - delta
+			
+			newCellOffSet = max(newCellOffSet, 0)
+			mainScrollView?.setContentOffset(CGPoint(x: 0, y: newCellOffSet), animated: false)
+			scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: false)
+		}
+	}
+	
+	func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+		endScrolling(scrollView)
+	}
+	
+	func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+		endScrolling(scrollView)
+	}
+	
+	private func endScrolling(_ scrollView: UIScrollView) {
+		//handling ketika user end scrolling
+		let yContentOffSet = scrollView.contentOffset.y
+		let cellHeight: CGFloat = 44
+		
+		if yContentOffSet < cellHeight {
+			mainScrollView?.isScrollEnabled = true
+			scrollView.isScrollEnabled = false
+		}
+		
+	}
 }
 
